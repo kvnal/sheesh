24543a461a0d271022d2b3e68c59a3e4537be185
kvnal <72142540+kvnal@users.noreply.github.com>
2022-04-15 19:24:17 +0530

    stories added

---
+++ client/src/Components/Components/Cards/Story.js
@@ -0,0 +1,28 @@
+
+import React, { Component } from 'react'
+
+import Stories from '@reactrondev/react-stories';
+
+class Story extends Component {
+    render() {
+        return (
+            <Stories
+                stories={stories}
+                defaultInterval={1500}
+                width={432}
+                height={768}
+            />
+        );
+    }
+}
+
+const stories = [
+    { url: 'https://picsum.photos/1080/1920', seeMore: <span>hello world</span>,
+     header: { heading: 'Mohit Karekar', subheading: 'Posted 5h ago', profileImage: 'https://picsum.photos/1000/1000' } }
+     , { url: 'https://fsa.zobj.net/crop.php?r=dyJ08vhfPsUL3UkJ2aFaLo1LK5lhjA_5o6qEmWe7CW6P4bdk5Se2tYqxc8M3tcgYCwKp0IAyf0cmw9yCmOviFYb5JteeZgYClrug_bvSGgQxKGEUjH9H3s7PS9fQa3rpK3DN3nx-qA-mf6XN', 
+     header: { heading: 'Mohit Karekar', subheading: 'Posted 32m ago', profileImage: 'https://picsum.photos/1080/1920' } },
+      { url: 'https://media.idownloadblog.com/wp-content/uploads/2016/04/iPhone-wallpaper-abstract-portrait-stars-macinmac.jpg', 
+      header: { heading: 'reactrondev/@reactrondev/react-stories', subheading: 'Posted 32m ago', profileImage: 'https://avatars0.githubusercontent.com/u/24852829?s=400&v=4' } },
+]   
+
+export default Story;
--- client/src/Components/Pages/Home.js
+++ client/src/Components/Pages/Home.js
@@ -5,6 +5,7 @@ import { FaFileUpload } from "react-icons/fa";
 import {MdAddBox} from 'react-icons/md'
 import {FaUserAlt} from 'react-icons/fa'
 import { Link } from 'react-router-dom';
+import Story from '../Components/Cards/Story';
 const fileUploadPORT = "4000";
 
 const Home = () => {
@@ -29,13 +30,17 @@ const Home = () => {
                 </Link>
             </div>
 
-            <div className='mt-6 flex justify-center items-center'>
+
+                    <div className='scale-90 absolute top-16 story-css'> 
+                    <Story/>
+                    </div>
+            {/* <div className='mt-6 flex justify-center items-center'>
                 <Swipe props={ [
       "https://i.pinimg.com/236x/06/75/9d/06759dc8472db17acb9414eec9991460.jpg",
        "https://pm1.narvii.com/6737/47e81761302ce90b1138fea05c90b32a1f7c5b98v2_hq.jpg",
         "https://i.pinimg.com/736x/ec/63/47/ec63478a8128cbce816e3ebf281dbda7.jpg"
     ]} btn={false} />
-            </div>
+            </div> */}
         </div>
 
     ); 
