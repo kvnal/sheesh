93e3e73b0b0c935da44d284fabacaa4416087115
kvnal <72142540+kvnal@users.noreply.github.com>
2022-04-16 17:56:01 +0530

    sheesh live - symbl.ai + twilio live video

---
+++ sheesh-live-symbl-twilio-video/src/theme.js
@@ -0,0 +1,15 @@
+import { createMuiTheme } from '@material-ui/core';
+
+export default createMuiTheme({
+    palette: {
+        type: 'light',
+        primary: {
+            main: '#F30059',
+        },
+        secondary: {
+            main: '#F30059'
+        },
+    },
+    sidebarWidth: 260,
+    sidebarMobileHeight: 90,
+});
